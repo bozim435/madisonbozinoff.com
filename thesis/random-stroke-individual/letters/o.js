83f8ba0548eea2c1258932e175df8f77dc3a6743
Madison <bozim435@newschool.edu>
2018-04-05 17:15:55 -0400

    typeface animations

---
+++ thesis/random-stroke-individual/letters/o.js
@@ -0,0 +1,17 @@
+function draw_o(max) {
+
+	strokeWeight(random(0, window.innerWidth / 20));
+	
+	// quarters of the canvas
+	var q1 = max / 4;
+	var q2 = max / 2;
+	var q3 = (max / 4) * 3;
+
+	// coordinates
+	var h = random(5, max);
+	var w = random(5, max);
+
+	// circle
+	ellipse(q2, q2, w, h);
+
+}
---
+++ thesis/random-stroke-individual/sketch.js
@@ -0,0 +1,203 @@
+function setup() {
+
+	createCanvas(window.innerWidth, window.innerHeight * 2);
+	frameRate(4);
+
+}
+
+function draw() {
+
+	background(255);
+
+	noFill();
+	stroke(0);
+
+	// dimensions
+	var l_w_h = window.innerWidth / 10.5;
+	var line_height = 1.2;
+
+	if (window.innerWidth <= 768) {
+
+		l_w_h = window.innerWidth / 5.5;
+
+		// draw letters
+		translate(25, 25);
+		draw_t(l_w_h); 
+		translate(l_w_h, 0);
+		draw_h(l_w_h); 
+		translate(l_w_h, 0);
+		draw_e(l_w_h); 
+		translate(0 - l_w_h * 2, l_w_h * line_height); 
+
+		// space
+		draw_q(l_w_h); 
+		translate(l_w_h, 0);
+		draw_u(l_w_h); 
+		translate(l_w_h, 0);
+		draw_i(l_w_h); 
+		translate(l_w_h, 0);
+		draw_c(l_w_h); 
+		translate(l_w_h, 0);
+		draw_k(l_w_h); 
+		translate(0 - l_w_h * 4, l_w_h * line_height);
+
+		// line break
+		draw_b(l_w_h); 
+		translate(l_w_h, 0);
+		draw_r(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_w(l_w_h); 
+		translate(l_w_h, 0);
+		draw_n(l_w_h); 
+		translate(0 - l_w_h * 4, l_w_h * line_height);
+
+		// space
+		draw_f(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_x(l_w_h); 
+		translate(0 - l_w_h * 2, l_w_h * line_height);
+
+		// line break
+		draw_j(l_w_h); 
+		translate(l_w_h, 0);
+		draw_u(l_w_h); 
+		translate(l_w_h, 0);
+		draw_m(l_w_h); 
+		translate(l_w_h, 0);
+		draw_p(l_w_h); 
+		translate(l_w_h, 0);
+		draw_s(l_w_h); 
+		translate(0 - l_w_h * 4, l_w_h * line_height);
+
+		// space
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_v(l_w_h); 
+		translate(l_w_h, 0);
+		draw_e(l_w_h); 
+		translate(l_w_h, 0);
+		draw_r(l_w_h); 
+		translate(0 - l_w_h * 3, l_w_h * line_height);
+
+		// line break
+		draw_t(l_w_h); 
+		translate(l_w_h, 0);
+		draw_h(l_w_h);
+		translate(l_w_h, 0);
+		draw_e(l_w_h);
+		translate(0 - l_w_h * 2, l_w_h * line_height); 
+
+		// space 
+		draw_l(l_w_h); 
+		translate(l_w_h, 0);
+		draw_a(l_w_h); 
+		translate(l_w_h, 0);
+		draw_z(l_w_h); 
+		translate(l_w_h, 0);
+		draw_y(l_w_h); 
+		translate(0 - l_w_h * 3, l_w_h * line_height);
+
+		// line break
+		draw_d(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_g(l_w_h);
+
+	} else {
+
+		// draw letters
+		translate(25, 25);
+		draw_t(l_w_h); 
+		translate(l_w_h, 0);
+		draw_h(l_w_h); 
+		translate(l_w_h, 0);
+		draw_e(l_w_h); 
+		translate(l_w_h * 2, 0); 
+
+		// space
+		draw_q(l_w_h); 
+		translate(l_w_h, 0);
+		draw_u(l_w_h); 
+		translate(l_w_h, 0);
+		draw_i(l_w_h); 
+		translate(l_w_h, 0);
+		draw_c(l_w_h); 
+		translate(l_w_h, 0);
+		draw_k(l_w_h); 
+		translate(0 - l_w_h * 8, l_w_h * line_height);
+
+		// line break
+		draw_b(l_w_h); 
+		translate(l_w_h, 0);
+		draw_r(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_w(l_w_h); 
+		translate(l_w_h, 0);
+		draw_n(l_w_h); 
+		translate(l_w_h * 2, 0);
+
+		// space
+		draw_f(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_x(l_w_h); 
+		translate(0 - l_w_h * 8, l_w_h * line_height);
+
+		// line break
+		draw_j(l_w_h); 
+		translate(l_w_h, 0);
+		draw_u(l_w_h); 
+		translate(l_w_h, 0);
+		draw_m(l_w_h); 
+		translate(l_w_h, 0);
+		draw_p(l_w_h); 
+		translate(l_w_h, 0);
+		draw_s(l_w_h); 
+		translate(l_w_h * 2, 0);
+
+		// space
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_v(l_w_h); 
+		translate(l_w_h, 0);
+		draw_e(l_w_h); 
+		translate(l_w_h, 0);
+		draw_r(l_w_h); 
+		translate(0 - l_w_h * 9, l_w_h * line_height);
+
+		// line break
+		draw_t(l_w_h); 
+		translate(l_w_h, 0);
+		draw_h(l_w_h);
+		translate(l_w_h, 0);
+		draw_e(l_w_h);
+		translate(l_w_h * 2, 0); 
+
+		// space 
+		draw_l(l_w_h); 
+		translate(l_w_h, 0);
+		draw_a(l_w_h); 
+		translate(l_w_h, 0);
+		draw_z(l_w_h); 
+		translate(l_w_h, 0);
+		draw_y(l_w_h); 
+		translate(0 - l_w_h * 7, l_w_h * line_height);
+
+		// // line break
+		draw_d(l_w_h); 
+		translate(l_w_h, 0);
+		draw_o(l_w_h); 
+		translate(l_w_h, 0);
+		draw_g(l_w_h);
+
+	}
+
+}
