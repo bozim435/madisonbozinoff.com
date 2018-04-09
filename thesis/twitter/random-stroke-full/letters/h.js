c8b0f44253ec3355729f10a96f980d1d5df200a5
Madison <bozim435@newschool.edu>
2018-04-09 02:44:43 -0400

    uploaded tweet fetcher sketches

---
+++ thesis/twitter/random-stroke-full/letters/h.js
@@ -0,0 +1,20 @@
+function draw_h(max) {
+
+	// quarters of the canvas
+	var q1 = max / 4;
+	var q2 = max / 2;
+	var q3 = (max / 4) * 3;
+
+	// coordinates
+	var xA = random(5, q2);
+	var yA = random(5, q2);
+	var yB = random(q2, max);
+	var yC = ((yB - yA) / 2) + yA;
+	var xD = random(q2, max);
+
+	// lines
+	line(xA, yA, xA, yB);
+	line(xA, yC, xD, yC);
+	line(xD, yA, xD, yB);
+
+}
---
+++ thesis/twitter/random-stroke-full/letters/i.js
@@ -0,0 +1,24 @@
+function draw_i(max) {
+
+	// quarters of the canvas
+	var q1 = max / 4;
+	var q2 = max / 2;
+	var q3 = (max / 4) * 3;
+
+	// coordinates	
+	var xA = random(5, q2);
+	var yA = random(5, q1);
+	var xB = random(q2, max);
+	
+	// vertical line
+	line(xA, yA, xB, yA);
+
+	// secondary coordinates
+	var xC = ((xB - xA) / 2) + xA;
+	var yD = random(yA, max);
+
+	// horizontal lines
+	line(xC, yA, xC, yD);
+	line(xA, yD, xB, yD);
+
+}
