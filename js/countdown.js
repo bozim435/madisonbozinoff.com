cc4a508180e4675b18d1fe6b151d4201625efee8
Madison <bozim435@newschool.edu>
2018-08-15 22:45:43 -0400

    let and const???

--- js/countdown.js
+++ js/countdown.js
@@ -1,14 +1,23 @@
-var deadline = new Date("Aug 19, 2018 24:00:00").getTime();
+/*** Coundown ***/
+
+const deadline = new Date("Aug 19, 2018 24:00:00").getTime();
 
 setInterval(function() {
 
-    var current = new Date().getTime();
-    var difference = deadline - current;		    
+    let current = new Date().getTime();
+    let difference = deadline - current;
+    // console.log(difference);		    
 
-    var h = Math.floor(difference / (1000 * 60 * 60));
-    var m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var s = Math.floor((difference % (1000 * 60)) / 1000);
+    let h = Math.floor(difference / (1000 * 60 * 60));
+    let m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((difference % (1000 * 60)) / 1000);
     
     document.getElementById("countdown").innerHTML = h + " hours, " + m + " minutes, " + s + " seconds ";
+
+    if (difference <= 0) {
+
+    	alert("Your bitch ass better be done >:o");
+
+    }
     
 }, 1000)
